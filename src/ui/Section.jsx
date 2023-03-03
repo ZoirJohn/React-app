8c7724b427c7b823055123ac24c706a5224e60ca
ZoirJohn <zoirbekzokirjonov2007@gmailcom>
2023-03-03 21:28:43 +0400

    last commit in this day

--- src/ui/Section.jsx
+++ src/ui/Section.jsx
@@ -1,20 +1,20 @@
-import img from "../img/background.webp";
-import dev from "../img/photo.png";
+import img from '../img/background.webp';
+import dev from '../img/photo.png';
 
 const ProfileSection = (props) => {
     return (
-        <section className="profile">
-            <img src={img} alt="background" />
-            <div className="card">
-                <img src={props.profile.photos.small || dev} alt="profileImage" />
-                <div className=" card-text">
-                    <p className="card-name">{props.profile.fullName}</p>
-                    <p className="card-birthday">Date of birth: 1998</p>
-                    <p className="card-city">City: Minsk</p>
-                    <p className="card-education">Education: BSU'11 </p>
+        <section className='profile'>
+            <img src={img} alt='background' />
+            <div className='card'>
+                <img src={props.profile.photos.large || dev} alt='profileImage' />
+                <div className=' card-text'>
+                    <p className='card-name'>{props.profile.fullName}</p>
+                    <p className='card-birthday'>Date of birth: 1998</p>
+                    <p className='card-city'>City: Minsk</p>
+                    <p className='card-education'>Education: BSU'11 </p>
                     <p>
-                        Web-site:{" "}
-                        <a className="card-site" href="https://github.com" target="_blank">
+                        Web-site:{' '}
+                        <a className='card-site' href='https://github.com' target='_blank'>
                             https://github.com
                         </a>
                     </p>
