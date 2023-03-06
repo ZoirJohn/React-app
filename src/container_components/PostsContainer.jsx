333fae7a97ae7c5f295de6d28521b7293c75b2dd
ZoirJohn <zoirbekzokirjonov2007@gmailcom>
2023-03-06 19:02:28 +0400

    prettier

--- src/container_components/PostsContainer.jsx
+++ src/container_components/PostsContainer.jsx
@@ -3,18 +3,18 @@ import { addPostAction, updatePostAction } from '../redux/profile-reducer';
 import Posts from '../ui/Posts';
 
 const mapStateToProps = (state) => {
-   return { post: state.profilePage.post, message: state.profilePage.message };
+    return { post: state.profilePage.post, message: state.profilePage.message };
 };
 
 const mapDispatchToProps = (dispatch) => {
-   return {
-      updatePost: (text) => {
-         dispatch(updatePostAction(text));
-      },
-      addPost: () => {
-         dispatch(addPostAction());
-      },
-   };
+    return {
+        updatePost: (text) => {
+            dispatch(updatePostAction(text));
+        },
+        addPost: () => {
+            dispatch(addPostAction());
+        },
+    };
 };
 const PostsContainer = connect(mapStateToProps, mapDispatchToProps)(Posts);
 
--- src/ui/Posts.jsx
+++ src/ui/Posts.jsx
@@ -3,31 +3,31 @@ import styles from '../css/Posts.module.css';
 import React from 'react';
 
 const Posts = (props) => {
-   let text = React.createRef();
+    let text = React.createRef();
 
-   const addText = () => {
-      props.addPost();
-   };
+    const addText = () => {
+        props.addPost();
+    };
 
-   const addOnChangePost = () => {
-      props.updatePost(text.current.value);
-   };
+    const addOnChangePost = () => {
+        props.updatePost(text.current.value);
+    };
 
-   return (
-      <section className={styles.section__post}>
-         <div className={styles.form}>
-            <label>My posts</label>
-            <textarea placeholder='Введите текст' onChange={addOnChangePost} value={props.post} ref={text}></textarea>
+    return (
+        <section className={styles.section__post}>
+            <div className={styles.form}>
+                <label>My posts</label>
+                <textarea placeholder='Введите текст' onChange={addOnChangePost} value={props.post} ref={text}></textarea>
 
-            <button onClick={addText}>Send</button>
-         </div>
+                <button onClick={addText}>Send</button>
+            </div>
 
-         <div className={styles.posts_box}>
-            {props.message.map((e) => (
-               <Post message={e.text} key={e.id} />
-            ))}
-         </div>
-      </section>
-   );
+            <div className={styles.posts_box}>
+                {props.message.map((e) => (
+                    <Post message={e.text} key={e.id} />
+                ))}
+            </div>
+        </section>
+    );
 };
 export default Posts;
