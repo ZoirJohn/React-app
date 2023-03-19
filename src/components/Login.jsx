e9fe724f7e92340829b864586abb5804a63f8782
ZoirJohn <zoirbekzokirjonov2007@gmailcom>
2023-03-19 17:03:54 +0500

    redux-form was installed

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,35 @@
+import { reduxForm } from 'redux-form';
+import styles from '../css/Login.module.css';
+
+const Form = () => {
+    return (
+        <form action='news' className={styles.form}>
+            <label htmlFor='email'>
+                Email: <input type='email' id='email' />
+            </label>
+
+            <label htmlFor='password'>
+                Password: <input type='password' id='password' />
+            </label>
+
+            <label htmlFor='remember'>
+                Remember me: <input type='checkbox' id='remember' />
+            </label>
+
+            <button>Login</button>
+        </form>
+    );
+};
+
+const ReduxForm = reduxForm({
+    form: 'login',
+})(Form);
+
 const Login = () => {
     return (
         <section>
             <h1>Login</h1>
+            <ReduxForm />
         </section>
     );
 };
--- src/redux/redux.js
+++ src/redux/redux.js
@@ -3,13 +3,15 @@ import { profile_reducer } from './profile-reducer';
 import { messages_reducer } from './messages-reducer';
 import { users_redcuer } from './users-reducer';
 import { auth_reducer } from './auth-reducer';
-import thunkMiddleware  from 'redux-thunk';
+import { reducer as form_reducer } from 'redux-form';
+import thunkMiddleware from 'redux-thunk';
 
 const reducers = combineReducers({
     profilePage: profile_reducer,
     messagesPage: messages_reducer,
     usersPage: users_redcuer,
     auth: auth_reducer,
+    form: form_reducer,
 });
 
 const store = legacy_createStore(reducers, applyMiddleware(thunkMiddleware));
