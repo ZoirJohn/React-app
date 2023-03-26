6240dc99c725af30fc3ecb02633304826b3430eb
ZoirJohn <zoirbekzokirjonov2007@gmailcom>
2023-03-26 05:56:55 +0500

    textarea is working

--- src/ui/Textarea.jsx
+++ src/ui/Textarea.jsx
@@ -1,6 +1,12 @@
 import styles from '../css/Tools.module.css';
 
 const Textarea = ({ input, meta, ...props }) => {
-    return <textarea {...input} {...props} className={styles.text_area}></textarea>;
+    const error = meta.touched && meta.error;
+    return (
+        <div className={styles.text_area__parent}>
+            <textarea {...input} {...props} className={`${styles.text_area} ${error ? styles.error : ''}`}></textarea>
+            <span className={styles.error__text}>{error}</span>
+        </div>
+    );
 };
 export default Textarea;
